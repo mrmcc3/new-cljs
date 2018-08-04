19807a2011e5fd51eb39b008f3658acf5f0af472
mrmcc3 <mrmcc3@gmail.com>
2018-08-04 20:29:24 +1000

    more changes

--- src/clj/new/new_cljs.clj
+++ src/clj/new/new_cljs.clj
@@ -2,11 +2,11 @@
   (:require [clj.new.templates :as tpl]))
 
 (defn new-cljs [name]
-  (let [name (tpl/project-name name)
-        render (tpl/renderer "new-cljs")
-        data {:name name :sanitized (tpl/name-to-path name)}]
+  (let [render (tpl/renderer "new-cljs")
+        data {:name      (tpl/project-name name)
+              :sanitized (tpl/name-to-path name)}]
     (tpl/->files data
-             ["deps.edn" (render "deps.edn" data)]
-             ["shadow-cljs.edn" (render "shadow-cljs.edn" data)]
-             ["src/{{sanitized}}/app.cljs" (render "app.cljs" data)]
-             ["target/app/index.html" (render "index.html" data)])))
+                 ["deps.edn" (render "deps.edn" data)]
+                 ["shadow-cljs.edn" (render "shadow-cljs.edn" data)]
+                 ["src/{{sanitized}}/app.cljs" (render "app.cljs" data)]
+                 ["target/app/index.html" (render "index.html" data)])))
