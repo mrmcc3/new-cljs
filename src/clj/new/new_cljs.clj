45d574eb601df0122021a6c61d07fd46393b684d
mrmcc3 <mrmcc3@gmail.com>
2018-08-04 21:27:22 +1000

    have to use :name

--- src/clj/new/new_cljs.clj
+++ src/clj/new/new_cljs.clj
@@ -3,10 +3,10 @@
 
 (defn new-cljs [name]
   (let [render (tpl/renderer "new-cljs")
-        data {:path (tpl/name-to-path name)
-              :ns   (tpl/sanitize-ns name)}]
+        data   {:name (tpl/name-to-path name)
+                :ns   (tpl/sanitize-ns name)}]
     (tpl/->files data
                  ["deps.edn" (render "deps.edn" data)]
                  ["shadow-cljs.edn" (render "shadow-cljs.edn" data)]
-                 ["src/{{path}}/app.cljs" (render "app.cljs" data)]
+                 ["src/{{name}}/app.cljs" (render "app.cljs" data)]
                  ["target/app/index.html" (render "index.html" data)])))
